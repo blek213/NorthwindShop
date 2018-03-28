03914f3a60e4d5f6c067e4972b2ff5c457917d69
blek213 <blek213@gmail.com>
2018-03-28 12:58:47 +0300

    Pagination has done

--- NorthwindShop/Controllers/HomeController.cs
+++ NorthwindShop/Controllers/HomeController.cs
@@ -3,23 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using NorthwindShop.Models;
+using PagedList.Mvc;
+using PagedList;
 namespace NorthwindShop.Controllers
 {
     public class HomeController : Controller
     {
+        ShopContext DbShop = new ShopContext();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Confections()
+        public ActionResult Confections(int? page)
         {
-            return View();
+            
+            IEnumerable<Products> confections = DbShop.Database.SqlQuery<Products>("SELECT * FROM Products WHERE CategoryID = 3");
+
+            int confectionsCount = 0;
+            foreach(var b in confections)
+            {
+                confectionsCount++;
+            }
+
+            int pageSize = 15;
+            int pageCount = (confectionsCount) / (pageSize);
+            int pageNumber = (page ?? 1);
+
+            return View(confections.ToPagedList(pageNumber,pageSize));
         }
-        public ActionResult Beverages()
+        public ActionResult Beverages(int? page)
         {
-            return View();
+            IEnumerable<Products> beverages = DbShop.Database.SqlQuery<Products>("SELECT * FROM Products WHERE CategoryID = 1");
+
+            int confectionsCount = 0;
+            foreach (var b in beverages)
+            {
+                confectionsCount++;
+            }
+
+            int pageSize = 15;
+            int pageCount = (confectionsCount) / (pageSize);
+            int pageNumber = (page ?? 1);
+
+            return View(beverages.ToPagedList(pageNumber,pageSize));
         }
 
     }
