69b5797b0673763c5a25b3569ed1c7329c2d1b3a
blek213 <blek213@gmail.com>
2018-03-31 21:45:07 +0300

    Added a window shopping cart. It's not finished yet

---
+++ NorthwindShop/Scripts/CustomScripts/WindowScript.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('.popup .close_window, .overlay').click(function () {
+        $('.popup, .overlay').css({ 'opacity': '0', 'visibility': 'hidden' });
+    });
+    $('input.open_window').click(function (e) {
+        $('.popup, .overlay').css({ 'opacity': '1', 'visibility': 'visible' });
+        e.preventDefault();
+    });
+    $('#plusButton').click(function (e) {
+        var getVal = $('#InputText').val();
+        getVal++;
+        $('#InputText').val(String(getVal));
+    });
+    $('#minusButton').click(function (e) {
+        var getVal = $('#InputText').val();
+        if (getVal != 0) {
+            getVal--;
+            $('#InputText').val(String(getVal));
+        }
+
+    });
+});
+
