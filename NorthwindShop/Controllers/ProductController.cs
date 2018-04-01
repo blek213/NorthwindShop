e2f82b8efef5ef2eae94877729afe42b91426e44
blek213 <blek213@gmail.com>
2018-04-01 15:33:02 +0300

    Shopping window has successfully added

--- NorthwindShop/Controllers/ProductController.cs
+++ NorthwindShop/Controllers/ProductController.cs
@@ -1,9 +1,11 @@
 ﻿using NorthwindShop.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +15,8 @@ namespace NorthwindShop.Controllers
     public class ProductController : Controller
     {
         ShopContext DbShop = new ShopContext();
+
+        CookieContainer cookieContainer = new CookieContainer();
 
         public ActionResult Confection(int? Id)
         {
@@ -48,27 +52,72 @@ namespace NorthwindShop.Controllers
 
         public ActionResult Cart()
         {
-            HttpCookie cookieProduct= Request.Cookies["Product"];
+            
+            if(Request.Cookies["Product"] != null)
+            {
+                HttpCookie cookieProduct = Request.Cookies["Product"];
 
-            int cookieValue = Int32.Parse(cookieProduct.Value);
+                //Distribution our cookie value in two values: 1) ProductId 2) Count
 
-            var FoundModel = DbShop.Products.First(p => p.ProductID == cookieValue);
-            ViewBag.ourProduct = FoundModel;
+                string str = cookieProduct.Value;
 
+                int positionStr = str.IndexOf("/");
+
+                string IdValue = "";
+
+                for (int i = 0; i < positionStr; i++)
+                {
+                    IdValue += str[i];
+                }
+
+                positionStr++;
+
+                string CountValue = "";
+
+                for (int i = positionStr; i < str.Length; i++)
+                {
+                    CountValue += str[i];
+                }
+
+                int ProductId = Int32.Parse(IdValue);
+
+                int Count = Int32.Parse(CountValue);
+
+                //Found our model by ProductId
+
+                var FoundModel = DbShop.Products.First(p => p.ProductID == ProductId);
+
+                //Representing our values for client
+
+                ViewBag.ourProduct = FoundModel;
+
+                ViewBag.Count = Count;
+            }
+               
             return View();
         }
+       
 
         [HttpPost]
-        public ActionResult Cart(int? Id)
+        public ActionResult Cart(int? IdProductSet,int? InputText)
         {
-            var Product = DbShop.Products.First(p => p.ProductID == Id);
 
-            HttpCookie cookieProduct = new HttpCookie("Product",Id.ToString());
+            var Product = DbShop.Products.First(p => p.ProductID == IdProductSet);
+            
+            HttpCookie cookieProduct = new HttpCookie("Product", IdProductSet.ToString() + "/" + InputText.ToString());
 
             Response.Cookies.Add(cookieProduct);
 
-            return RedirectToAction("Confection", "Product", new { Id = Id });
+            return RedirectToAction("Confection", "Product", new { Id = IdProductSet });
         }
-    
+
+        public ActionResult AddToCart(int? IdProductFromView)
+        {
+            var Product = DbShop.Products.First(p => p.ProductID == IdProductFromView);
+
+            return PartialView(Product);
+        }
+
+
     }
 }
--- NorthwindShop/Scripts/CustomScripts/WindowScript.js
+++ NorthwindShop/Scripts/CustomScripts/WindowScript.js
@@ -1,10 +1,16 @@
 ﻿$(document).ready(function () {
+
     $('.popup .close_window, .overlay').click(function () {
         $('.popup, .overlay').css({ 'opacity': '0', 'visibility': 'hidden' });
     });
+
     $('input.open_window').click(function (e) {
         $('.popup, .overlay').css({ 'opacity': '1', 'visibility': 'visible' });
         e.preventDefault();
+
+        var hiddenId = $('#hiddenProductId').val();
+        hiddenId = encodeURIComponent(hiddenId);
+        $('#ProductNameInWindow').load("http://localhost:56567/Product/AddToCart?IdProductFromView=" + hiddenId);
     });
     $('#plusButton').click(function (e) {
         var getVal = $('#InputText').val();
@@ -13,11 +19,13 @@
     });
     $('#minusButton').click(function (e) {
         var getVal = $('#InputText').val();
-        if (getVal != 0) {
+        if (getVal != 1) {
             getVal--;
             $('#InputText').val(String(getVal));
         }
 
     });
 });
+
+
 
